043dc1ba10e14f9352a305d04744d305a7466c7b
ddizoya <ddizoya@danielcastelao.org>
2015-12-11 12:19:32 +0100

    * En general, hasta ahora, he cambiado la compatibilidad de la API 23 con la API18, en Module Settings > Flavours.
    * He añadido el botón en layout del fragment. Lo he tenido que meter todo dentro de un LinearLayout con orientación vertical, para añadir el texto.
    
    * He creado el botón en la clase ItemDetailActivity para borrar el contenido del fragment, y posteriormente finalizar la ejecución de esta segunda activity.

---
+++ ItemDetailFragment.java
@@ -0,0 +1,90 @@
+package com.example.ddizoya.examen;
+
+import android.app.Activity;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.ddizoya.examen.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Item detail screen.
+ * This fragment is either contained in a {@link ItemListActivity}
+ * in two-pane mode (on tablets) or a {@link ItemDetailActivity}
+ * on handsets.
+ */
+public class ItemDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public ItemDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mItem.content);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.details);
+        }
+
+        // Creamos un objeto botón con la ID del botón del fragment, para así manejarlo.
+        Button bBorradoFragment = (Button)  rootView.findViewById(R.id.botonFragment);
+
+        // Le asociamos un listener para que, cada vez que se clickee, se borren todos los elementos que muestra el TextView del fragment.
+        bBorradoFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                 //Ahora, el TextView borrará todo su contenido por "Borrado"
+                ((TextView) rootView.findViewById(R.id.item_detail)).setText("Borrado");
+
+                // Y se cierra la activity padre.
+                getActivity().finish();
+
+            }
+        });
+
+
+
+        return rootView;
+    }
+}
