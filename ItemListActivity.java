f0920e033c1343bed46cf870d10df8231fccebb2
ddizoya <ddizoya@danielcastelao.org>
2015-12-11 12:55:38 +0100

    * He cambiado el startActivity por startActivityForResult, pasándole junto con el intent, una constante con valor 1. Luego, en su método onActivityResult() correspondiente, he hecho que se imprima la Toast diciendo "OK".

---
+++ ItemListActivity.java
@@ -0,0 +1,102 @@
+package com.example.ddizoya.examen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.Toast;
+
+
+
+public class ItemListActivity extends AppCompatActivity
+        implements ItemListFragment.Callbacks {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+    //Creamos una constante de tipo int que valide el valor enviado por el startActivityForResult()
+    private static final int OK = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_app_bar);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        if (findViewById(R.id.item_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-large and
+            // res/values-sw600dp). If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+
+            // In two-pane mode, list items should be given the
+            // 'activated' state when touched.
+            ((ItemListFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.item_list))
+                    .setActivateOnItemClick(true);
+        }
+
+        // TODO: If exposing deep links into your app, handle intents here.
+    }
+
+    /**
+     * Callback method from {@link ItemListFragment.Callbacks}
+     * indicating that the item with the given ID was selected.
+     */
+    @Override
+    public void onItemSelected(String id) {
+        if (mTwoPane) {
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            Bundle arguments = new Bundle();
+            arguments.putString(ItemDetailFragment.ARG_ITEM_ID, id);
+            ItemDetailFragment fragment = new ItemDetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.item_detail_container, fragment)
+                    .commit();
+
+        } else {
+            // In single-pane mode, simply start the detail activity
+            // for the selected item ID.
+            Intent detailIntent = new Intent(this, ItemDetailActivity.class);
+            detailIntent.putExtra(ItemDetailFragment.ARG_ITEM_ID, id);
+
+            //Empleamos el método startActivityForResult(), y le pasamos el intent y la constante que creamos previamente como atributo (valor int 1)
+            startActivityForResult(detailIntent, OK);
+        }
+    }
+
+    //Una vez abrimos el detalle de alguien, y le damos al botón de Borrar que hay allí, se cerrará la 2ª activity y saldrá la toast.
+    //Este método siempre retorna el dato cuando la activity a la que se llama se 'muere'. Es importante que se cierre.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OK){
+            Toast.makeText(this,  "OK", Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+
+    }
+
+
